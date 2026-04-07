2a9b27787d0fc6f3a0ecc49f4d8d23a135f607bb
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-04-08 03:25:30 +0600

    Explore more context API

--- src/component/FamilyTree/Aunty.jsx
+++ src/component/FamilyTree/Aunty.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { use } from 'react';
 import Cousin from './Cousin';
+import { MoneyContext } from './FamilyTree';
 
 const Aunty = ({asset}) => {
+  const [money,setMoney]=use(MoneyContext);
   return (
     <div>
       <h2>Aunty</h2>
@@ -9,6 +11,7 @@ const Aunty = ({asset}) => {
         <Cousin name='Faiza' asset={asset}></Cousin>
         <Cousin name='Rabi'></Cousin>
         <Cousin name='Shahi'></Cousin>
+        <button onClick={()=>{setMoney(money+5000)}}>Add 5000$</button>
       </section>
       
     </div>
